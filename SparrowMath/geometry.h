1debf0153a5d6ca84951a508ed288b85f8e3ba4f
r000setta <1509248091@qq.com>
2020-09-29 18:56:36 +0800

    add normal3

--- SparrowMath/geometry.h
+++ SparrowMath/geometry.h
@@ -130,6 +130,10 @@ namespace sparrow {
 			DCHECK(!HasNaNs());
 		}
 
+		Vector3<T>(const Normal3<T>& n) : x(n.x), y(n.y), z(n.z) {
+			DCHECK(!n.HasNaNs());
+		}
+
 		Vector3(const Vector3<T>& v) {
 			DCHECK(!v.HasNaNs());
 			x = v.x;
@@ -499,6 +503,121 @@ namespace sparrow {
 	using Point3f = Point3<Float>;
 	using Point3i = Point3<int>;
 
+	template<typename T>
+	class Normal3 {
+	public:
+		T x, y, z;
+		bool HasNaNs() const { return isNaN(x) || isNaN(y) || isNaN(z); }
+
+		Normal3() :x(0), y(0), z(0) {}
+		Normal3(T xx, T yy, T zz) :x(xx), y(yy), z(zz) {
+			DCHECK(!HasNaNs());
+		}
+		Normal3<T> operator-() const {
+			return Normal3(-x, -y, -z);
+		}
+		Normal3<T> operator+(const Normal3<T>& n) const {
+			DCHECK(!n.HasNaNs());
+			return Normal3<T>(x + n.x, y + n.y, z + n.z);
+		}
+		Normal3<T>& operator+=(const Normal3<T>& n) {
+			DCHECK(!n.HasNaNs());
+			x += n.x;
+			y += n.y;
+			z += n.z;
+			return *this;
+		}
+		Normal3<T> operator-(const Normal3<T>& n) const {
+			DCHECK(!n.HasNaNs());
+			return Normal3<T>(x - n.x, y - n.y, z - n.z);
+		}
+
+		Normal3<T>& operator-=(const Normal3<T>& n) {
+			DCHECK(!n.HasNaNs());
+			x -= n.x;
+			y -= n.y;
+			z -= n.z;
+			return *this;
+		}
+		template<typename U>
+		Normal3<T> operator*(U f) const {
+			return Normal3<T>(f * x, f * y, f * z);
+		}
+
+		template <typename U>
+		Normal3<T>& operator*=(U f) {
+			x *= f;
+			y *= f;
+			z *= f;
+			return *this;
+		}
+		template <typename U>
+		Normal3<T> operator/(U f) const {
+			CHECK_NE(f, 0);
+			Float inv = (Float)1 / f;
+			return Normal3<T>(x * inv, y * inv, z * inv);
+		}
+
+		template <typename U>
+		Normal3<T>& operator/=(U f) {
+			CHECK_NE(f, 0);
+			Float inv = (Float)1 / f;
+			x *= inv;
+			y *= inv;
+			z *= inv;
+			return *this;
+		}
+		Float LengthSquared() const { return x * x + y * y + z * z; }
+		Float Length() const { return std::sqrt(LengthSquared()); }
+
+		Normal3<T>(const Normal3<T>& n) {
+			DCHECK(!n.HasNaNs());
+			x = n.x;
+			y = n.y;
+			z = n.z;
+		}
+
+		Normal3<T>& operator=(const Normal3<T>& n) {
+			DCHECK(!n.HasNaNs());
+			x = n.x;
+			y = n.y;
+			z = n.z;
+			return *this;
+		}
+
+		explicit Normal3<T>(const Vector3<T>& v) : x(v.x), y(v.y), z(v.z) {
+			DCHECK(!v.HasNaNs());
+		}
+		bool operator==(const Normal3<T>& n) const {
+			return x == n.x && y == n.y && z == n.z;
+		}
+		bool operator!=(const Normal3<T>& n) const {
+			return x != n.x || y != n.y || z != n.z;
+		}
+
+		T operator[](int i) const {
+			DCHECK(i >= 0 && i <= 2);
+			if (i == 0) return x;
+			if (i == 1) return y;
+			return z;
+		}
+
+		T& operator[](int i) {
+			DCHECK(i >= 0 && i <= 2);
+			if (i == 0) return x;
+			if (i == 1) return y;
+			return z;
+		}
+	};
+
+	template <typename T>
+	inline std::ostream& operator<<(std::ostream& os, const Normal3<T>& v) {
+		os << "[ " << v.x << "," << v.y << "," << v.z << " ]";
+		return os;
+	}
+
+	using Normal3f = Normal3<Float>;
+
 	//Vector3
 	template<typename T>
 	Vector3<T> Abs(const Vector3<T>& v) {
@@ -524,6 +643,23 @@ namespace sparrow {
 			(v1.x * v2.y) - (v1.y * v2.x));
 	}
 
+	template <typename T>
+	inline Vector3<T> Cross(const Vector3<T>& v1, const Normal3<T>& v2) {
+		DCHECK(!v1.HasNaNs() && !v2.HasNaNs());
+		double v1x = v1.x, v1y = v1.y, v1z = v1.z;
+		double v2x = v2.x, v2y = v2.y, v2z = v2.z;
+		return Vector3<T>((v1y * v2z) - (v1z * v2y), (v1z * v2x) - (v1x * v2z),
+			(v1x * v2y) - (v1y * v2x));
+	}
+
+	template <typename T>
+	inline Vector3<T> Cross(const Normal3<T>& v1, const Vector3<T>& v2) {
+		DCHECK(!v1.HasNaNs() && !v2.HasNaNs());
+		double v1x = v1.x, v1y = v1.y, v1z = v1.z;
+		double v2x = v2.x, v2y = v2.y, v2z = v2.z;
+		return Vector3<T>((v1y * v2z) - (v1z * v2y), (v1z * v2x) - (v1x * v2z),
+	}
+
 	template<typename T>
 	inline Vector3<T> Normalize(const Vector3<T>& v) {
 		return v / v.Length();
@@ -539,6 +675,28 @@ namespace sparrow {
 		return std::fmax(v.x, std::fmax(v.y, v.z));
 	}
 
+	template <typename T>
+	int MaxDimension(const Vector3<T>& v) {
+		return (v.x > v.y) ? ((v.x > v.z) ? 0 : 2) : ((v.y > v.z) ? 1 : 2);
+	}
+
+	template <typename T>
+	Vector3<T> Min(const Vector3<T>& p1, const Vector3<T>& p2) {
+		return Vector3<T>(std::min(p1.x, p2.x), std::min(p1.y, p2.y),
+			std::min(p1.z, p2.z));
+	}
+
+	template <typename T>
+	Vector3<T> Max(const Vector3<T>& p1, const Vector3<T>& p2) {
+		return Vector3<T>(std::max(p1.x, p2.x), std::max(p1.y, p2.y),
+			std::max(p1.z, p2.z));
+	}
+
+	template<typename T>
+	Vector3<T> Permute(const Vector3<T>& v, int x, int y, int z) {
+		return Vector3<T>(v[x], v[y], v[z]);
+	}
+
 	//Vector2
 	template<typename T>
 	Vector2<T> Abs(const Vector2<T>& v) {
@@ -546,13 +704,13 @@ namespace sparrow {
 	}
 
 	template<typename T>
-	inline T Dot(const Vector2<T>& v1, const Vector2<T>& v2) {
+	inline Float Dot(const Vector2<T>& v1, const Vector2<T>& v2) {
 		DCHECK(!v1.HasNaNs() && !v2.HasNaNs());
 		return v1.x * v2.x + v1.y * v2.y;
 	}
 
 	template<typename T>
-	inline T AbsDot(const Vector2<T>& v1, const Vector2<T>& v2) {
+	inline Float AbsDot(const Vector2<T>& v1, const Vector2<T>& v2) {
 		DCHECK(!v1.HasNaNs && !v2.HasNaNs());
 		return std::abs(Dot(v1, v2));
 	}
@@ -560,5 +718,182 @@ namespace sparrow {
 	template<typename T>
 	inline Vector2<T> Normalize(const Vector2<T>& v) {
 		return v / v.Length();
+	}
+
+	template<typename T,typename U>
+	inline Vector2<T> operator*(U f, const Vector2<T>& v) {
+		return v * f;
+	}
+
+	template<typename T>
+	Vector2<T> Abs(const Vector2<T>& v) {
+		return Vector2<T>(std::abs(v.x), std::abs(v.y));
+	}
+
+	//Point
+	template<typename T>
+	inline Float Distance(const Point3<T>& p1, const Point3<T>& p2) {
+		return (p1 - p2).Length();
+	}
+
+	template<typename T>
+	inline Float DistanceSquared(const Point3<T>& p1, const Point3<T>& p2) {
+		return (p1 - p2).LengthSquared();
+	}
+
+	template<typename T,typename U>
+	inline Point3<T> operator*(U f, const Point3<T>& p) {
+		DCHECK(!p.HasNaNs());
+		return p * f;
+	}
+
+	template<typename T>
+	Point3<T> Lerp(Float t, const Point3<T>& p0, const Point3<T>& p1) {
+		return (1 - t) * p0 + t * p1;
+	}
+
+	template<typename T>
+	Point3<T> Min(const Point3<T>& p1, const Point3<T>& p2) {
+		return Point3<T>(std::min(p1.x, p2.x), std::min(p1.y, p2.y),
+			std::min(p1.z, p2.z));
+	}
+
+	template <typename T>
+	Point3<T> Max(const Point3<T>& p1, const Point3<T>& p2) {
+		return Point3<T>(std::max(p1.x, p2.x), std::max(p1.y, p2.y),
+			std::max(p1.z, p2.z));
+	}
+
+	template <typename T>
+	Point3<T> Floor(const Point3<T>& p) {
+		return Point3<T>(std::floor(p.x), std::floor(p.y), std::floor(p.z));
+	}
+
+	template <typename T>
+	Point3<T> Ceil(const Point3<T>& p) {
+		return Point3<T>(std::ceil(p.x), std::ceil(p.y), std::ceil(p.z));
+	}
+
+	template <typename T>
+	Point3<T> Abs(const Point3<T>& p) {
+		return Point3<T>(std::abs(p.x), std::abs(p.y), std::abs(p.z));
+	}
+
+	template <typename T>
+	inline Float Distance(const Point2<T>& p1, const Point2<T>& p2) {
+		return (p1 - p2).Length();
+	}
+
+	template <typename T>
+	inline Float DistanceSquared(const Point2<T>& p1, const Point2<T>& p2) {
+		return (p1 - p2).LengthSquared();
+	}
+
+	template <typename T, typename U>
+	inline Point2<T> operator*(U f, const Point2<T>& p) {
+		DCHECK(!p.HasNaNs());
+		return p * f;
+	}
+
+	template <typename T>
+	Point2<T> Floor(const Point2<T>& p) {
+		return Point2<T>(std::floor(p.x), std::floor(p.y));
+	}
+
+	template <typename T>
+	Point2<T> Ceil(const Point2<T>& p) {
+		return Point2<T>(std::ceil(p.x), std::ceil(p.y));
+	}
+
+	template <typename T>
+	Point2<T> Lerp(Float t, const Point2<T>& v0, const Point2<T>& v1) {
+		return (1 - t) * v0 + t * v1;
+	}
+
+	template <typename T>
+	Point2<T> Min(const Point2<T>& pa, const Point2<T>& pb) {
+		return Point2<T>(std::min(pa.x, pb.x), std::min(pa.y, pb.y));
+	}
+
+	template <typename T>
+	Point2<T> Max(const Point2<T>& pa, const Point2<T>& pb) {
+		return Point2<T>(std::max(pa.x, pb.x), std::max(pa.y, pb.y));
+	}
+
+	template <typename T>
+	Point3<T> Permute(const Point3<T>& p, int x, int y, int z) {
+		return Point3<T>(p[x], p[y], p[z]);
+	}
+
+	//Normal3
+	template<typename T,typename U>
+	inline Normal3<T> operator*(U f, const Normal3<T>& n) {
+		return Normal3<T>(f * n.x, f * n.y, f * n.z);
+	}
+
+	template <typename T>
+	inline Normal3<T> Normalize(const Normal3<T>& n) {
+		return n / n.Length();
+	}
+
+	template <typename T>
+	inline T Dot(const Normal3<T>& n1, const Vector3<T>& v2) {
+		DCHECK(!n1.HasNaNs() && !v2.HasNaNs());
+		return n1.x * v2.x + n1.y * v2.y + n1.z * v2.z;
+	}
+
+	template <typename T>
+	inline T Dot(const Vector3<T>& v1, const Normal3<T>& n2) {
+		DCHECK(!v1.HasNaNs() && !n2.HasNaNs());
+		return v1.x * n2.x + v1.y * n2.y + v1.z * n2.z;
+	}
+
+	template <typename T>
+	inline T Dot(const Normal3<T>& n1, const Normal3<T>& n2) {
+		DCHECK(!n1.HasNaNs() && !n2.HasNaNs());
+		return n1.x * n2.x + n1.y * n2.y + n1.z * n2.z;
+	}
+
+	template <typename T>
+	inline T AbsDot(const Normal3<T>& n1, const Vector3<T>& v2) {
+		DCHECK(!n1.HasNaNs() && !v2.HasNaNs());
+		return std::abs(n1.x * v2.x + n1.y * v2.y + n1.z * v2.z);
+	}
+
+	template <typename T>
+	inline T AbsDot(const Vector3<T>& v1, const Normal3<T>& n2) {
+		DCHECK(!v1.HasNaNs() && !n2.HasNaNs());
+		return std::abs(v1.x * n2.x + v1.y * n2.y + v1.z * n2.z);
+	}
+
+	template <typename T>
+	inline T AbsDot(const Normal3<T>& n1, const Normal3<T>& n2) {
+		DCHECK(!n1.HasNaNs() && !n2.HasNaNs());
+		return std::abs(n1.x * n2.x + n1.y * n2.y + n1.z * n2.z);
+	}
+
+	template <typename T>
+	inline Normal3<T> Faceforward(const Normal3<T>& n, const Vector3<T>& v) {
+		return (Dot(n, v) < 0.f) ? -n : n;
+	}
+
+	template <typename T>
+	inline Normal3<T> Faceforward(const Normal3<T>& n, const Normal3<T>& n2) {
+		return (Dot(n, n2) < 0.f) ? -n : n;
+	}
+
+	template <typename T>
+	inline Vector3<T> Faceforward(const Vector3<T>& v, const Vector3<T>& v2) {
+		return (Dot(v, v2) < 0.f) ? -v : v;
+	}
+
+	template <typename T>
+	inline Vector3<T> Faceforward(const Vector3<T>& v, const Normal3<T>& n2) {
+		return (Dot(v, n2) < 0.f) ? -v : v;
+	}
+
+	template <typename T>
+	Normal3<T> Abs(const Normal3<T>& v) {
+		return Normal3<T>(std::abs(v.x), std::abs(v.y), std::abs(v.z));
 	}
 }
